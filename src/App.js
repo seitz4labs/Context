e3eee22ae25fe7c66a0a0646f51dcffdad00326d
Paulverizer <pseufzer@gmail.com>
2020-12-29 12:00:09 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import "./styles.css";
 import Sub1 from "./Sub1";
 import Control from "./Control";
 import MyProv from "./MyProv";
+import marked from "marked";
 
 export default function App() {
   console.log("App");
+  const mdText = marked("# title \n<b>Hi</b>", false);
   return (
     <div className="App">
       <MyProv>
-        <h1>Start editing to see some magic happen!</h1>
+        <h1>Start editing to see some magic happen today!</h1>
         <Sub1 />
         <Control />
       </MyProv>
+      <div dangerouslySetInnerHTML={{ __html: mdText }} />
     </div>
   );
 }
